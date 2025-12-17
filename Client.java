2e6d78f03788c55eab0986bce4e197e77d0e87be
loliva-progr <l.oliva@istitutocorni.it>
2025-12-17 11:21:00 +0000

    first commit

---
+++ Client.java
@@ -0,0 +1,6 @@
+public class Client {
+    public static void main(String[] args) {
+        // Stampa un messaggio sulla console
+        System.out.println("Ciao client, mondo!");
+    }
+}
---
+++ Server.java
@@ -0,0 +1,6 @@
+public class Server{
+    public static void main(String[] args) {
+        // Stampa un messaggio sulla console
+        System.out.println("Ciao, mondo!");
+    }
+}
